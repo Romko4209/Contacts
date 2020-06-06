45bdd32d9186b90d59b5d3db368f4b737a3d99b9
Roman <romaha48915@gmail.com>
2020-06-06 21:06:39 +0300

    connect tableview

---
+++ Contacts/Extension /Extension + ContactsVC.swift
@@ -0,0 +1,23 @@
+//
+//  Extension + ContactsVC.swift
+//  Contacts
+//
+//  Created by Romko on 06.06.2020.
+//  Copyright © 2020 Romko. All rights reserved.
+//
+
+import  UIKit
+
+extension ContactsViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+}
---
+++ Contacts/View Controller/ContactsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ContactsViewController.swift
+//  Contacts
+//
+//  Created by Romko on 06.06.2020.
+//  Copyright © 2020 Romko. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var contacts = ["test","test","test","test","test","test","test",]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        
+    }
+    
+
+}
+
