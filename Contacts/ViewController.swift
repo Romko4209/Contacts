dcef88e2e6e730a86f4f5e3ca9494d89c6a592ce
Roman <romaha48915@gmail.com>
2020-06-06 20:44:47 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Romko on 06.06.2020.
+//  Copyright © 2020 Romko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
